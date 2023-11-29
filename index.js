455524914222ed5f10717c334015f49fc1f5b271
kajal-2011 <kajalrekha888@gmail.com>
2023-11-29 20:18:37 +0600

    products routes added

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-const express = require("express");
 require("dotenv").config();
 
+const express = require("express");
+const productRoutes = require("./routes/productRoutes");
+
 const app = express();
+
+// port
 const PORT = process.env.PORT || 4000;
 
+// middlewares
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// test api
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the luminex server!" });
+});
+
+// BYPASS API
+app.use("/api/products", productRoutes);
 
 // Start the server
 app.listen(PORT, () => {
---
+++ routes/productRoutes.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+// router
+const router = express.Router();
+
+// GET all products
+router.get("/", (req, res) => {
+  res.json({ message: "GET all products" });
+});
+
+// GET a single product
+router.get("/:id", (req, res) => {
+  res.json({ message: "GET a single product" });
+});
+
+// POST a new product
+router.post("/", (req, res) => {
+  res.json({ message: "POST a new product" });
+});
+
+// DELETE a product
+router.delete("/", (req, res) => {
+  res.json({ message: "DELETE a product" });
+});
+
+// UPDATE a product
+router.patch("/", (req, res) => {
+  res.json({ message: "UPDATE a product" });
+});
+module.exports = router;
