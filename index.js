afba07ce92f10327e4dbea73d915a84040628420
kajal-2011 <kajalrekha888@gmail.com>
2023-12-01 10:24:38 +0600

    login and signup routes added

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 require("dotenv").config();
-
+const cors = require("cors");
 const express = require("express");
 const mongoose = require("mongoose");
 const productRoutes = require("./routes/productRoutes");
+const userRoutes = require("./routes/userRoutes");
 
+
+// express app
 const app = express();
 
 // port
 const port = process.env.PORT || 4000;
 
 // middlewares
+app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
   console.log(req.path, req.method);
@@ -23,8 +27,7 @@ app.get("/", (req, res) => {
 
 // BYPASS API
 app.use("/api/products", productRoutes);
-
-//
+app.use("/api/user", userRoutes);
 
 // mongodb
 mongoose.set("strictQuery", false);
