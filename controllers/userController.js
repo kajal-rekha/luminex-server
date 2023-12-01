218fcabef4be835909ef37073ef5b5b559f1756e
kajal-2011 <kajalrekha888@gmail.com>
2023-12-01 23:05:13 +0600

    get a single user, update and delete user controller done

--- controllers/userController.js
+++ controllers/userController.js
@@ -47,4 +47,69 @@ const getAllUsers = async (req, res) => {
     res.status(400).json({ error: error.message });
   }
 };
-module.exports = { loginUser, createUser, getAllUsers };
+
+// Get a user
+const getAUser = async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// Update user by ID
+const updateUser = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const { name, email, password, image, address, occupation } = req.body;
+
+    const updateUser = await User.findByIdAndUpdate(
+      userId,
+      { name, email, password, image, address, occupation },
+      { new: true }
+    );
+
+    if (!updateUser) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.status(200).json(updateUser);
+  } catch (error) {
+    console.error("Error updating user:", error.message);
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// Delete user by ID
+const deleteUser = async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    const deletedUser = await User.findByIdAndDelete(userId);
+
+    if (!deletedUser) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.status(200).json({ message: "User deleted successfully", deletedUser });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  loginUser,
+  createUser,
+  getAllUsers,
+  getAUser,
+  updateUser,
+  deleteUser,
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { loginUser, createUser, getAllUsers } = require("../controllers/userController");
+const { loginUser, createUser, getAllUsers, updateUser, deleteUser, getAUser } = require("../controllers/userController");
 const { isAdmin } = require("../middlewares/admin");
 const { isAuthenticated } = require("../middlewares/auth");
 
@@ -15,5 +15,15 @@ router.post("/auth/login", loginUser);
 
 // get all users
 router.get("/", isAuthenticated, isAdmin, getAllUsers);
+
+// Get a user 
+router.get("/:userId", isAuthenticated, isAdmin, getAUser);
+
+
+// Update a user 
+router.put("/:userId", isAuthenticated, isAdmin, updateUser);
+
+// Delete a user 
+router.delete("/:userId", isAuthenticated, isAdmin, deleteUser);
 
 module.exports = router;
