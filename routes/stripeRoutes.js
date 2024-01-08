a59115c81a8dfc3f1bebdf1defb40b1236d34fc8
kajal-2011 <kajalrekha888@gmail.com>
2024-01-08 18:04:30 +0600

    done

--- routes/stripeRoutes.js
+++ routes/stripeRoutes.js
@@ -8,28 +8,33 @@ const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 const router = express.Router();
 
 router.post("/create-checkout-session", async (req, res) => {
-  const session = await stripe.checkout.sessions.create({
-    line_items: [
-      {
-        price_data: {
-          currency: "usd",
-          product_data: {
-            name: "T-shirt",
+  const line_items = req.body.cartItems.map((item) => {
+    return {
+      price_data: {
+        currency: "usd",
+        product_data: {
+          name: item.name,
+          images: [item.image],
+          description: item.desc,
+          metadata: {
+            id: item.id,
           },
-          unit_amount: 2000,
         },
-        quantity: 1,
+        unit_amount: item.price * 100,
       },
-    ],
+      quantity: item.cartQuantity,
+    };
+  });
+
+  const session = await stripe.checkout.sessions.create({
+    line_items,
     mode: "payment",
     success_url: "http://localhost:3000/checkout-success",
     cancel_url: "http://localhost:3000/cart",
   });
 
- console.log("Stripe Session:", session);
- res.send({ url: session.url });
-
+  console.log("Stripe Session:", session);
+  res.send({ url: session.url });
 });
-
 
 module.exports = router;
